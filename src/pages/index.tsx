2fbdfb8bff56f0aed11ff4a9b7e24da9fc71d238
Theo Browne <me@t3.gg>
2022-08-30 23:14:26 -0700

    metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Get set up in OBS and your Ping.gg room!"
     >
       <HomepageHeader />
       <main>{/* <HomepageFeatures /> */}</main>
